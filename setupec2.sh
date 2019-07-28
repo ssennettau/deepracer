bd0115399166df945a27f9433828fa90ac82e08c
Stephen Sennett <ssennett@ssennett.net>
2019-07-28 22:27:25 +1000

    Fixed git clones and minio setup

--- setupec2.sh
+++ setupec2.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 # Clone ncessary git repositories
-mkdir ~/deepracer
-cd ~/deepracer
-https://github.com/ssennettau/deepracer.git
+mkdir ~/ssennettau
+cd ~/ssennettau
+git clone --recurse-submodules https://github.com/ssennettau/deepracer.git
 mkdir ~/crr0004
 cd ~/crr0004
 git clone --recurse-submodules https://github.com/crr0004/deepracer.git
 cd ~
 
-# Pull and Run Minio in background
+# Pull and run Minio in background
 cat ~/crr0004/deepracer/rl_coach/env.sh | sed --expression='s/export //g' > ~/miniovars.list
-docker run -p 9000:9000 --env-file ~/miniovars.list -v /mnt/data:/data ~/minio/minio server /data > /dev/null 2>&1 &
+docker pull minio/minio
+docker run -p 9000:9000 --env-file ~/miniovars.list -v /mnt/data:/data minio/minio server /data > /dev/null 2>&1 &
+echo Waiting for minio to start...
+sleep 5
+mkdir /mnt/data/bucket
 
 # Getting Public IP
 echo
