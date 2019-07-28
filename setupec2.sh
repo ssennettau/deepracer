b0fdf64e9783ccb3378c50c5375401400ca04d37
Stephen Sennett <ssennett@ssennett.net>
2019-07-28 21:53:42 +1000

    Changed Minio URL Display

--- setupec2.sh
+++ setupec2.sh
@@ -13,5 +13,7 @@ cat ~/crr0004/deepracer/rl_coach/env.sh | sed --expression='s/export //g' > ~/mi
 docker run -p 9000:9000 --env-file ~/miniovars.list -v /mnt/data:/data ~/minio/minio server /data > /dev/null 2>&1 &
 
 # Getting Public IP
-curl http://169.254.169.254/latest/meta-data/public-ipv4
+echo
+echo --- Minio Running ---
+echo http://$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4):9000
 echo
