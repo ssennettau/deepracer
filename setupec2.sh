9261c951713fe41ec31a503eb32f7b130f05e747
Stephen Sennett <ssennett@ssennett.net>
2019-07-28 21:30:29 +1000

    Add extra repo and folder structure

--- setupec2.sh
+++ setupec2.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-# Clone git repository
-cd ~
+# Clone ncessary git repositories
+mkdir ~/deepracer
+cd ~/deepracer
+https://github.com/ssennettau/deepracer.git
+mkdir ~/crr0004
+cd ~/crr0004
 git clone --recurse-submodules https://github.com/crr0004/deepracer.git
+cd ~
 
 # Pull and Run Minio in background
-cat deepracer/rl_coach/env.sh | sed --expression='s/export //g' > miniovars.list
-docker run -p 9000:9000 --env-file miniovars.list -v /mnt/data:/data minio/minio server /data > /dev/null 2>&1 &
+cat ~/crr0004/deepracer/rl_coach/env.sh | sed --expression='s/export //g' > ~/miniovars.list
+docker run -p 9000:9000 --env-file ~/miniovars.list -v /mnt/data:/data ~/minio/minio server /data > /dev/null 2>&1 &
 
 # Getting Public IP
 curl http://169.254.169.254/latest/meta-data/public-ipv4
-
+echo
